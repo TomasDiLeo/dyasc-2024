d3dc4e3b29ce18d6b86bc12921ff7eec88d96a3c
Tomas Di Leo <tomasjdle@gmail.com>
2024-08-16 02:13:28 -0300

    Mejora en el manejo de excepciones

--- fibo/src/main/java/ar/edu/untref/dyasc/Ejecutable.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Ejecutable.java
@@ -3,28 +3,24 @@ package ar.edu.untref.dyasc;
 import java.util.List;
 
 public class Ejecutable {
-    public static void main(String[] args) {
-    	if(args.length != 1) {
-    		System.err.println("Error: Exactly one argument is required.");
-            System.err.println("Usage: java -jar fibo.jar <n>");
-            System.exit(1);
-    	}
-    	FibonacciSequence fibo = new FibonacciSequence();
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("Error: Exactly one argument is required.");
+			System.err.println("Usage: java -jar fibo.jar <n>");
+			System.exit(1);
+		}
+		FibonacciSequence fibo = new FibonacciSequence();
 
-    	try {
-            int n = Integer.parseInt(args[0]);
-            if (n < 0) {
-                throw new IllegalArgumentException("The argument must be a non-negative integer.");
-            }
-
-            List<Integer> sequence = fibo.fibonacciSequence(n);
-            System.out.println("fibo <" + n + ">: " + sequence.toString());
-        } catch (NumberFormatException e) {
-            System.err.println("Error: Argument must be an integer.");
-            System.exit(1);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Error: " + e.getMessage());
-            System.exit(1);
-        }
-    }
+		try {
+			int n = Integer.parseInt(args[0]);
+			List<Integer> sequence = fibo.fibonacciSequence(n);
+			System.out.println("fibo <" + n + ">: " + sequence.toString());
+		} catch (NumberFormatException e) {
+			System.err.println("Error: Argument must be an integer.");
+			System.exit(1);
+		} catch (IllegalArgumentException e) {
+			System.err.println("Error: " + e.getMessage());
+			System.exit(1);
+		}
+	}
 }
--- fibo/src/main/java/ar/edu/untref/dyasc/FibonacciSequence.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/FibonacciSequence.java
@@ -6,19 +6,20 @@ import java.util.List;
 public class FibonacciSequence {
 	public List<Integer> fibonacciSequence(int n) {
 		List<Integer> sequence = new ArrayList<Integer>();
-		if (n <= 0) return sequence;
-		
+		if (n <= 0)
+			throw new IllegalArgumentException("The argument must be a positive integer");
+
 		int a = 0;
 		int b = 1;
-		
-		int aux; 
-		for(int i = 0; i < n; i++) {
+
+		int aux;
+		for (int i = 0; i < n; i++) {
 			sequence.add(a);
 			aux = a + b;
 			a = b;
 			b = aux;
 		}
-		
+
 		return sequence;
 	}
 }
